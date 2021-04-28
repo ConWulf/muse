cc6cdc984e4af95583d1811d9ea44bf6bf83cd4d
Connor Wulf <connor.m.wulf@gmail.com>
2021-04-28 11:43:25 -0500

    adds delete doc composable

---
+++ src/composables/UseDoc.js
@@ -0,0 +1,26 @@
+import {ref} from "vue"
+import {firestore} from "@/firebase/config"
+
+const useDoc = (collection, id) => {
+    const error = ref(null)
+    const isPending = ref(false)
+    let doc = firestore
+        .collection(collection)
+        .doc(id)
+
+    const deleteDoc = async () => {
+        error.value = null
+        isPending.value = true
+        try {
+            const resp = await doc.delete()
+            isPending.value = false
+            return resp
+        } catch {
+            error.value = "could not delete item."
+        }
+    }
+
+    return { error, isPending, deleteDoc }
+}
+
+export default useDoc
--- src/views/playlists/Details.vue
+++ src/views/playlists/Details.vue
@@ -1,29 +1,47 @@
 <template>
-  <div>
+  <div class="grid grid-cols-3 gap-x-16 justify-between px-4 mt-20">
     <div v-if="error">{{error}}</div>
-    <div v-if="playlist">
+    <div v-if="playlist" class="col-span-full sm:col-span-1">
 <!--playlist info-->
-      <div>
-        <div class="w-72">
-          <img :src="playlist.url" alt="cover image">
+      <div class="text-center">
+        <div class="max-w-md relative overflow-hidden p-32 rounded-md">
+          <img :src="playlist.url" alt="cover image" class=" min-w-full min-h-full absolute inset-0">
         </div>
-        <div>{{ playlist.title }}</div>
-        <p>by {{playlist.username}}</p>
-        <p>{{playlist.description }}</p>
+        <h2 class="text-xl font-bold text-gray-700">{{ playlist.title }}</h2>
+        <p class="text-sm">by {{playlist.username}}</p>
+        <p class="text-sm text-left">{{playlist.description }}</p>
+        <button v-if="owner" @click="removePlaylist">Delete</button>
       </div>
     </div>
+    <div class="sm:col-span-2 col-span-full">songs</div>
   </div>
 </template>
 
 <script>
-import getDoc from "@/composables/GetDoc";
+import getDoc from "@/composables/GetDoc"
+import getUser from "@/composables/GetUser"
+import useDoc from "@/composables/UseDoc";
+import {computed} from "vue";
 export default {
   name: "Details",
   props: ['id'],
   setup(props) {
     const {error, doc: playlist} = getDoc('playlists', props.id)
+    const { user } = getUser()
+    const {error: e, isPending, deleteDoc} = useDoc('playlists', playlist.value.id)
 
-    return {error, playlist}
+    const owner = computed(() => {
+      return playlist.value && user.value && user.value.uid === playlist.value.userId
+    })
+
+    const removePlaylist = async () => {
+      await deleteDoc()
+      if(!e) {
+        console.log('do something');
+      }
+    }
+
+    return {error, playlist, owner, removePlaylist, e, isPending}
   }
 }
 </script>
