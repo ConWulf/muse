97153726a7c2d2dd6bd72f57017c46ee7f7fd017
Connor Wulf <connor.m.wulf@gmail.com>
2021-05-03 10:50:32 -0500

    user can view their specific playists

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -1,15 +1,19 @@
 <template>
-<div>
-
+<div v-if="playlists">
+  <Playlists :playlists="playlists"/>
 </div>
 </template>
 
 <script>
 import getCollection from "@/composables/GetCollection"
-import getUser from "@/composables/GetUser";
+import getUser from "@/composables/GetUser"
+import Playlists from "@/components/Playlists";
 
 export default {
   name: "UserPlaylists",
+  components: {
+    Playlists
+  },
   setup() {
     const {user} = getUser()
     const {docs: playlists} = getCollection('playlists',
